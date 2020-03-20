49c20ffa36234b99dd34bba50b6d6265b960dec2
Udesh-Ranjan <ranjanudesh404@gmail.com>
2020-03-20 22:07:47 +0530

    Added Attachment Feature

--- src/mailsender/MailSender.java
+++ src/mailsender/MailSender.java
@@ -5,16 +5,29 @@ import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.Properties;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.activation.FileDataSource;
 import javax.mail.Authenticator;
+import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
+import javax.mail.Multipart;
 import javax.mail.PasswordAuthentication;
 import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -41,7 +54,7 @@ public class MailSender{
         JTextField _subject;
         
         public Mail(){
-            this.setBackground(Color.yellow);
+            //this.setBackground(Color.yellow);
             this.setLayout(new GridLayout(4,2,60,5));
             this.setSize(400,200);
             name=new JLabel("Sender Email");
@@ -65,19 +78,92 @@ public class MailSender{
     static class Body extends JPanel{
         JLabel body;
         JTextArea _body;
+        
+        JButton attachment;
+        JComboBox<Attachment>selectedAttachment;
         public Body(){
-            this.setBackground(Color.yellow);
-            setLayout(new FlowLayout(FlowLayout.CENTER));
-            this.setSize(700,200);
+            //this.setBackground(Color.yellow);
+            setLayout(new GridLayout(2,2,60,10));
+            this.setSize(400,50);
             
             body=new JLabel("Body");
             _body=new JTextArea(10,30);
+            
+            attachment=new JButton("Add Attachment");
+            selectedAttachment=new JComboBox<>();
+            selectedAttachment.setSize(10,20);
+            selectedAttachment.setEditable(true);
+            attachment.addActionListener(new ActionListener() {
+
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    
+                    JFileChooser fileChooser=new JFileChooser();
+                    fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+                    int result=fileChooser.showOpenDialog(body);
+                    if(result==JFileChooser.APPROVE_OPTION){
+                        System.out.println("Approved");
+                        try {
+                            File file=fileChooser.getSelectedFile();
+                            String path=file.getCanonicalPath();
+                            Attachment attachment=new Attachment(file);
+                            selectedAttachment.addItem(attachment);
+//                            System.out.println(attachment);
+//                            selectedAttachment.add("Hell",attachment);
+//                            System.out.println(selectedAttachment);
+                        } catch (IOException | MessagingException ex) {
+                            Logger.getLogger(MailSender.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        
+                    }
+                }
+            });
+            
+            JPanel panel1=new JPanel();
+            JPanel panel2=new JPanel();
+            
+            panel1.add(attachment);
+            panel2.add(selectedAttachment);
+            
             add(body);
             add(_body);
+            
+            add(panel1);
+            add(panel2);
+        }
+    }
+    static class Attachment{
+        String path;
+        BodyPart bodypart;
+        DataSource datasource;
+        File file;
+        public Attachment(File file) throws MessagingException, IOException {
+            this.file=file;
+            datasource=new FileDataSource(file.getAbsolutePath());
+            bodypart=new MimeBodyPart();
+            bodypart.setDataHandler(new DataHandler(datasource));
+            bodypart.setFileName(file.getAbsolutePath());
+            
+        }
+        public BodyPart getBodyPart(){
+            return bodypart;
+        }
+        @Override
+        public String toString(){
+            
+            String path=file.getAbsolutePath();
+            
+            for(int i=path.length()-1;i>=0;i--){
+                if(path.charAt(i)=='/'||path.charAt(i)=='\\'){
+                    return path.substring(i+1);
+                }
+            }
+            
+            return "";
         }
     }
     static class Butt extends JPanel{
-        JButton exit;
+        JButton reset;
         JButton send;
         
         Mail mail;
@@ -87,14 +173,14 @@ public class MailSender{
             this.mail=mail;
             this.body=body;
             
-            this.setBackground(Color.yellow);
+            //this.setBackground(Color.yellow);
             this.setLayout(new FlowLayout(FlowLayout.CENTER));
             this.setSize(700,200);
             
-            exit=new JButton("Reset");
+            reset=new JButton("Reset");
             send=new JButton("Send");
             
-            exit.addActionListener(new ActionListener() {
+            reset.addActionListener(new ActionListener() {
 
                 @Override
                 public void actionPerformed(ActionEvent e) {
@@ -104,6 +190,7 @@ public class MailSender{
                     mail._subject.setText("");
                     
                     body._body.setText("");
+                    body.selectedAttachment.removeAllItems();
                 }
             });
             
@@ -137,6 +224,14 @@ public class MailSender{
                         message.setRecipient(Message.RecipientType.TO,new InternetAddress(receiverMail));
                         message.setSubject(subject);
                         message.setText(content);
+                        
+                        Multipart multiPart=new MimeMultipart();
+                        if(body.selectedAttachment.getItemCount()!=0){
+                            for(int i=0;i<body.selectedAttachment.getItemCount();i++){
+                                multiPart.addBodyPart(body.selectedAttachment.getItemAt(i).getBodyPart());
+                            }
+                            message.setContent(multiPart);
+                        }
                         Transport.send(message);
                         send.setBackground(Color.green);
                     }
@@ -146,7 +241,7 @@ public class MailSender{
                     }
                 }
             });
-            add(exit);
+            add(reset);
             add(send);
         }
         
